dd138b67f2bd30f4af259e893fb3645721199960
Aditya1710S <aadityasonawane3@gmail.com>
2026-04-07 22:27:00 +0530

    added requirements.txt

--- app.py
+++ app.py
@@ -205,5 +205,7 @@ def get_summary():
     except Error as e:
         return jsonify({'error': str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+# if __name__ == '__main__':
+#     app.run(debug=True, port=5000)
+if __name__ == "__main__":
+    app.run()
